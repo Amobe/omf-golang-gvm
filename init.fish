a64efd325e1423831a5e6652877cf8e6fb61c00a
sunday <synchronized@yeah.net>
2019-09-07 10:24:43 +0800

    omf的gvm(golang version managerment)插件

---
+++ init.fish
@@ -0,0 +1,12 @@
+# gvm initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
+
+
+set -q GVM_ROOT; or set -l GVM_ROOT $HOME/.gvm
+
+fenv source {$GVM_ROOT}/scripts/gvm >/dev/null ^&1
+
